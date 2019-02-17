f5a78fb5db1986919b3619fdebecf2fb89d010a8
ws31wx <antonel-ernest.pazargic@ing.com>
2019-02-17 21:01:30 +0100

    [type script] Default properties and method parameters

--- src/ts/Interfaces.test.ts
+++ src/ts/Interfaces.test.ts
@@ -3,7 +3,13 @@ const productName = "Magic Keyboard 2";
 
 it("interface based person should work properly", () => {
   expect(magicKeyboard2.name).toBe(productName);
-  expect(order.getTotalPrice(10)).toBe(81)
+  expect(order2.getTotalPrice(10)).toBe(81);
+  expect(order2.dateAdd).toBeUndefined();
+  expect(order3.getTotalPrice()).toBe(180);
+  expect(order3.getTotalPrice(10)).toBe(162);
+
+  const currentDate = Date();
+  expect(order3.dateAdd || currentDate).toBe(currentDate);
 });
 
 interface IProduct {
@@ -14,8 +20,9 @@ interface IProduct {
 
 interface IOrderDetails {
   product: IProduct,
-  quantity: number
-  getTotalPrice(discount: number): number,
+  quantity: number,
+  dateAdd?: Date,
+  getTotalPrice(discount?: number): number,
 }
 
 const magicKeyboard2: IProduct = {
@@ -23,12 +30,18 @@ const magicKeyboard2: IProduct = {
   unitPrice: 90
 };
 
-
-
-const order: IOrderDetails = {
+const order2: IOrderDetails = {
   product: magicKeyboard2,
   quantity: 1,
   getTotalPrice(discount: number): number {
     return this.quantity * this.product.unitPrice * (100 - discount) / 100;
   }
 };
+
+const order3: IOrderDetails = {
+  product: magicKeyboard2,
+  quantity: 2,
+  getTotalPrice(discount?: number): number {
+    return this.product.unitPrice * this.quantity * (100 - (discount || 0)) / 100;
+  }
+}
